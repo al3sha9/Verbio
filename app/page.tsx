411b63ef29bc811c1040d915ad8255c5cc9e22fa
Ali Shan <optimist.alishan@gmail.com>
2025-03-20 04:10:16 +0500

    done with authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
   return (
-    <>
+    <section className="p-10">
       <h1>hello world</h1>
-      <Button size="lg" variant="destructive">
-        HELLO WORLD
-      </Button>
-    </>
+      <div className="space-x-2">
+        {session ? (
+          <LogoutLink>
+            <Button size="lg" className="cursor-pointer" variant="destructive">
+              Logout
+            </Button>
+          </LogoutLink>
+        ) : (
+          <>
+            <LoginLink>
+              <Button size="lg" className="cursor-pointer" variant="default">
+                Login
+              </Button>
+            </LoginLink>
+            <RegisterLink>
+              <Button size="lg" className="cursor-pointer" variant="secondary">
+                SIgn in
+              </Button>
+            </RegisterLink>
+          </>
+        )}
+      </div>
+    </section>
   );
 }
